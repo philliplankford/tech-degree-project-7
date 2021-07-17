6b2f79ce202acf208ca97a07343185eb99e44ebb
Phillip <Flip.dlankford@gmail.com>
2021-07-16 17:09:26 -0700

    js members object array

---
+++ js/app.js
@@ -0,0 +1,26 @@
+let members = [ 
+    {
+        profilePicture: '../images/member-1',
+        name: 'Victoria Chambers', 
+        email: 'victoria.chambers80@example.com',
+        dateJoined: '10/15/20'
+    },
+    {
+        profilePicture: '../images/member-2',
+        name: 'Dale Byrd', 
+        email: 'dale.byrd52@example.com',
+        dateJoined: '10/15/20'
+    },
+    {
+        profilePicture: '../images/member-3',
+        name: 'Dawn Wood', 
+        email: 'dawn.wood16@example.com',
+        dateJoined: '10/15/20'
+    },
+    {
+        profilePicture: '../images/member-4',
+        name: 'Dan Oliver', 
+        email: 'dan.oliver@example.com',
+        dateJoined: '10/15/20'
+    }
+];
