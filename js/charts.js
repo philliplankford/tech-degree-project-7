f210c3a90d2b42ef93b2b18733051a73eb8b98ab
Phillip <Flip.dlankford@gmail.com>
2021-07-21 14:52:48 -0700

    added all charts and chart styles

--- js/charts.js
+++ js/charts.js
@@ -1,82 +1,145 @@
 const trafficChart = document.getElementById('traffic-chart');
+const dailytrafficChart = document.getElementById('daily-traffic-chart');
+const mobileChart = document.getElementById('mobile-users-chart');
 
-const trafficLabels = {
-    hourly: {
-        labels: [
-        '16-22', 
-        '23-29', 
-        '30-5', 
-        '6-12', 
-        '13-19', 
-        '20-26', 
-        '27-3', 
-        '4-10', 
-        '11-17', 
-        '18-24', 
-        '25-31'],
-        data: [
-            550, 
-            1400, 
-            1000, 
-            2000, 
-            1500, 
-            1600, 
-            800, 
-            1700, 
-            2000, 
-            1000, 
-            2500],
-    },
-    daily: {
-        labels: ['yolo'],
-        data: [50],
-    },
-    weekly: {
-        labels: [],
-        data: [],
-    },
-    monthly: {
-        labels: [],
-        data: [],
+const trafficLabels = [
+    '16-22', 
+    '23-29', 
+    '30-5', 
+    '6-12', 
+    '13-19', 
+    '20-26', 
+    '27-3', 
+    '4-10', 
+    '11-17', 
+    '18-24', 
+    '25-31'
+];
+
+const trafficData = {
+    labels: trafficLabels,
+    datasets: [{
+        label: '',
+        backgroundColor: 'rgb(116, 119, 191, .5)',
+        borderColor: 'rgb(116, 119, 191, .5)',
+        data: [550, 1400, 1000, 2000, 1500, 1600, 800, 1700, 2000, 1000, 2500]
+    }]
+};
+
+const config = {
+    type: 'line',
+    data: trafficData,
+    options: {
+        plugins: {
+            legend: {
+                display: false,
+            }
+        },
+        scales: {
+            y: {
+                min: 0,
+                max: 2500,
+            }
+        },
+        elements: {
+            line: {
+                fill: true,
+                tension: .2,
+            }
+        }
     }
 }
 
-let selected = trafficLabels.hourly;
+const buildtrafficChart = new Chart( trafficChart, config);
+
 trafficNav = document.querySelector('.traffic-nav');
 trafficNav.addEventListener('click', (e) => { 
     // check if element is an li 
     let text = e.target.innerHTML;
     switch (text) {
         case 'Hourly':
-            selected = trafficLabels.hourly;
+            console.log(text);
             break;
         case 'Daily':
-            selected = trafficLabels.daily;
+            console.log(text);
             break;
         case 'Weekly':
-            selected = trafficLabels.weekly;
+            console.log(text);
             break;
         case 'Monthly':
-            selected = trafficLabels.monthly;
+            console.log(text);
             break;
         default: 
         break;
     }
-    
 });
 
-let trafficData = {
-    labels: selected.labels,
+// DAILY
+
+const dailyLabels = [
+    'S', 
+    'M', 
+    'T', 
+    'W', 
+    'T', 
+    'F', 
+    'S'
+];
+
+const dailyTraffic = {
+    labels: dailyLabels,
     datasets: [{
-        label: 'My First dataset',
-        backgroundColor: 'rgb(255, 99, 132)',
+        label: '',
+        backgroundColor: '#7477bf',
         borderColor: 'rgb(255, 99, 132)',
-        data: selected.data
+        data: [55, 105, 175, 120, 210, 200, 100]
     }]
 };
 
-const config = new Chart(trafficChart, {
-    type: 'line',
-    data: trafficData,
-    options: {}
-});
+const dailyConfig = {
+    type: 'bar',
+    data: dailyTraffic,
+    options: {
+        plugins: {
+            legend: {
+                display: false,
+            }
+        }
+    }
+}
+
+const builddailyChart = new Chart( dailytrafficChart, dailyConfig);
+
+// Mobile
+const mobileLabels= [
+    'Desktop', 
+    'Tablet', 
+    'Phones'
+];
+
+const mobileData= {
+    labels: mobileLabels,
+    datasets: [{
+        label: '',
+        backgroundColor: [
+            '#90ee90', 
+            '#7477bf',
+            '#51b6c8'
+        ],
+        data: [55, 105, 175]
+    }]
+};
+
+const mobileConfig = {
+    type: 'doughnut',
+    data: mobileData,
+    options: {
+        plugins: {
+            legend: {
+                position: 'right',
+            }
+        }
+    }
+}
+
+const buildmobileChart = new Chart( mobileChart, mobileConfig);
