c9d0eb721d6a67dd5d3617d11cadb1b553960f77
Phillip <Flip.dlankford@gmail.com>
2021-07-22 23:08:02 -0700

    alert dropdown

---
+++ js/dropdown.js
@@ -0,0 +1,21 @@
+const notifDropdown = document.getElementById('notif-dropdown');
+const bell = document.getElementById('bell');
+
+function toggleShow(element) {
+    element.classList.toggle('show');
+}
+
+bell.addEventListener('click', () => {
+    toggleShow(notifDropdown);
+})
+
+window.onclick = (e) => {
+    if (!e.target.matches('#bell')) {
+            if (notifDropdown.classList.contains('show')) {
+                notifDropdown.classList.remove('show');
+            }
+    }
+}
+
+
+
